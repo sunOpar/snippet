37d6ab5cd7472ab8765574fd2e465947903bdfc5
opar <xuyaguang0630@foxmail.com>
2020-02-09 15:44:26 +0800

    feat(immer): finish object case

--- immer/immer.js
+++ immer/immer.js
@@ -1,19 +1,54 @@
-const modifier = () => {
-  return {
-    set(target, propKey, value) {
-      Reflect.set(target, propKey, value)
+function isPlainObject(object) {
+  if (object === null || typeof object !== 'object') return false
+  const proto = Object.getPrototypeOf(object)
+  return proto === Object.prototype || proto === null
+}
+function produce(baseState, thunk) {
+  const copies = new Map()
+  const proxies = new Map()
+  const objectTrap = {
+    get(target, prop) {
+      return getOrCreateProxy(target[prop])
+    },
+    set(target, prop, value) {
+      if (target[prop] !== value) {
+        const copy = getOrCreateCopy(target)
+        copy[prop] = value
+      }
     },
   }
+  function getOrCreateProxy(object) {
+    if (!isPlainObject(object)) return object
+    if (proxies.get(object)) return proxies.get(object)
+    const proxy = new Proxy(object, objectTrap)
+    proxies.set(object, proxy)
+    return proxy
+  }
+  function getOrCreateCopy(object) {
+    if (!isPlainObject(object)) return object
+    if (copies.get(object)) return copies.get(object)
+    const copy = Object.assign({}, object)
+    copies.set(object, copy)
+    return copy
+  }
+  function hasObjectChange(object) {
+    if(!proxies.get(object)) return false
+    if(copies.get(object)) return true
+    // deep check
+    for(let i of Object.keys(object)) {
+      if(hasObjectChange(object[i])) return true
+    }
+  }
+  function finalizeObject(object){
+    if(!hasObjectChange(object)) return object
+    const copy = getOrCreateCopy(object)
+    Object.keys(copy).forEach(item=>{
+      copy[item] = finalizeObject(copy[item])
+    })
+    return copy
+  }
+  const proxy = getOrCreateProxy(baseState)
+  thunk(proxy)
+  return finalizeObject(baseState)
 }
-function produce(base, cb) {
-  const baseCopy = {...base}
-  const proxy = createProxy(baseCopy)
-  cb(proxy)
-  return baseCopy
-}
-function createProxy(base) {
-  const proxy = new Proxy(base, modifier)
-  return proxy
-}
-
 module.exports = produce
--- immer/immer.test.js
+++ immer/immer.test.js
@@ -1,11 +1,80 @@
-const produce = require('./immer')
+const immer = require('./immer')
 
-test('test { a: 1, c: {d:2} }, change a', () => {
-  const baseState = { a: 1, c: { d: 2 } }
-  const next = produce(baseState, draft => {
-    draft.a = 2
+describe('base', () => {
+  let baseState
+  let origBaseState
+
+  beforeEach(() => {
+    origBaseState = baseState = createBaseState()
   })
-  expect(baseState.a).toBe(1)
-  expect(next.a).toBe(2)
-  expect(baseState.c).toBe(next.c)
+
+  it('should return the original without modifications', () => {
+    const nextState = immer(baseState, () => {})
+    expect(nextState).toBe(baseState)
+  })
+
+  it('should return the original without modifications when reading stuff', () => {
+    const nextState = immer(baseState, s => {
+      expect(s.aProp).toBe('hi')
+      expect(s.anObject.nested).toEqual({ yummie: true })
+    })
+    expect(nextState).toBe(baseState)
+  })
+
+  it('should return a copy when modifying stuff', () => {
+    const nextState = immer(baseState, s => {
+      s.aProp = 'hello world'
+    })
+    expect(nextState).not.toBe(baseState)
+    expect(baseState.aProp).toBe('hi')
+    expect(nextState.aProp).toBe('hello world')
+    // structural sharing?
+    expect(nextState.nested).toBe(baseState.nested)
+  })
+
+  it('deep change bubbles up', () => {
+    const nextState = immer(baseState, s => {
+      s.anObject.nested.yummie = false
+    })
+    expect(nextState).not.toBe(baseState)
+    expect(nextState.anObject).not.toBe(baseState.anObject)
+    expect(baseState.anObject.nested.yummie).toBe(true)
+    expect(nextState.anObject.nested.yummie).toBe(false)
+    expect(nextState.anArray).toBe(baseState.anArray)
+  })
+
+  it('can add props', () => {
+    const nextState = immer(baseState, s => {
+      s.anObject.cookie = { tasty: true }
+    })
+    expect(nextState).not.toBe(baseState)
+    expect(nextState.anObject).not.toBe(baseState.anObject)
+    expect(nextState.anObject.nested).toBe(baseState.anObject.nested)
+    expect(nextState.anObject.cookie).toEqual({ tasty: true })
+  })
+
+  it('ignores single non-modification', () => {
+    const nextState = immer(baseState, s => {
+      s.aProp = 'hi'
+    })
+    expect(nextState).toBe(baseState)
+  })
+
+  afterEach(() => {
+    expect(baseState).toBe(origBaseState)
+    expect(baseState).toEqual(createBaseState())
+  })
 })
+
+function createBaseState() {
+  return {
+    anArray: [1, 2, { c: 3 }],
+    aProp: 'hi',
+    anObject: {
+      nested: {
+        yummie: true,
+      },
+      coffee: false,
+    },
+  }
+}
